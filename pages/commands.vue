c572dfcf113dcbcf84469d3daaa679082c0c4829
Evan Steitz <evan.steitz@tournamentkings.com>
2022-01-31 16:21:59 -0700

    add coming soon to commands

--- pages/commands.vue
+++ pages/commands.vue
@@ -1,31 +1,6 @@
 <template>
-  <div class="md:flex">
-    <table class="table-auto">
-      <thead>
-        <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th>Year</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-          <td>Malcolm Lockyer</td>
-          <td>1961</td>
-        </tr>
-        <tr>
-          <td>Witchy Woman</td>
-          <td>The Eagles</td>
-          <td>1972</td>
-        </tr>
-        <tr>
-          <td>Shining Star</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="flex justify-center">
+    <h1 class="text-secondary text-3xl mt-40">COMING SOON!</h1>
   </div>
 </template>
 
