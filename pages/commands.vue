ea2cbafcd34ae646eecff2baba481f9e360f3938
GitHubLockedMyOtherAccount <wh0spr0gamer@gmail.com>
2022-03-15 16:22:12 -0600

    Hello
    Added accordion and table to commands

--- pages/commands.vue
+++ pages/commands.vue
@@ -1,7 +1,203 @@
 <template>
-  <div class="md:flex">
-      <h1>Commands</h1>
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+      <title>Tabs</title>
+  </head>
+  <body>
+    <div class="mytabs">
+      <input type="radio" id="tabAll" name="mytabs" checked="checked">
+      <label for="tabAll">All</label>
+      <div class="tab">
+        <h2>All</h2>
+        <table>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
   </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+        </table>
+      </div>
+
+      <input type="radio" id="tabRacer" name="mytabs">
+      <label for="tabRacer">Racer</label>
+      <div class="tab">
+        <h2>Racer</h2>
+        <table>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+        </table>
+      </div>
+
+      <input type="radio" id="tabPlanet" name="mytabs">
+      <label for="tabPlanet">Planet</label>
+      <div class="tab">
+        <h2>Planet</h2>
+        <table>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+          <tr>
+            <template>
+  <div class="w-1/2 m-auto pt-10">
+    <Accordion title="consectetur adipiscing elit">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </Accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from './components/Accordion.vue'
+export default {
+  name: 'App',
+  components: {
+    Accordion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+          </tr>
+        </table>
+      </div>
+
+    </div>
+  </body>
+</html> 
 </template>
 
 <script>
@@ -28,4 +224,58 @@ export default {
 </script>
 
 <style>
+  body {
+    background: #ccc;
+    font-family: 'Roboto', sans-serif;
+}
+.mytabs {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 600px;
+    margin: 50px auto;
+    padding: 25px;
+}
+.mytabs input[type="radio"] {
+    display: none;
+}
+.mytabs label {
+    padding: 25px;
+    background: #e2e2e2;
+    font-weight: bold;
+}
+
+.mytabs .tab {
+    width: 100%;
+    padding: 20px;
+    background: #fff;
+    order: 1;
+    display: none;
+}
+.mytabs .tab h2 {
+    font-size: 3em;
+}
+
+.mytabs input[type='radio']:checked + label + .tab {
+    display: block;
+}
+
+.mytabs input[type="radio"]:checked + label {
+    background: #fff;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
 </style>
+
