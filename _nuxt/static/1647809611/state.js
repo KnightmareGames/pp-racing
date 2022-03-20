3874621f6ec024d2c53d1e5119f934835526873d
EckyYakov <ejsteitz@gmail.com>
2022-03-20 20:53:33 +0000

    Deploying to gh-pages from @ KnightmareGames/pp-racing@de162a8a0740064b4d21b665230b2d2dca6655ca 🚀

---
+++ _nuxt/static/1647809611/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647809611",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c708b64b"}}}}(null,"\u002F"));
